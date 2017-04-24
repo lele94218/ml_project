d30cd075d1b79c2de6c7f147c1ddcaac57ea1b71
lele94218 <terry.trxue@gmail.com>
2017-04-24 17:27:23 -0400

    add

--- hdbscan_t.py
+++ hdbscan_t.py
@@ -19,6 +19,11 @@ plt.scatter(test_data.T[0], test_data.T[1], color='b', **plot_kwds)
 
 clusterer = hdbscan.HDBSCAN(min_cluster_size=5, gen_min_span_tree=True)
 clusterer.fit(test_data)
+
+print(clusterer.labels_)
+print(clusterer.probabilities_)
+print(clusterer.cluster_persistence_)
+
 plt.figure()
 clusterer.minimum_spanning_tree_.plot(edge_cmap='viridis',
                                       edge_alpha=0.6,
@@ -30,5 +35,4 @@ clusterer.single_linkage_tree_.plot(cmap='viridis', colorbar=True)
 
 plt.figure()
 clusterer.condensed_tree_.plot(select_clusters=True, selection_palette=sns.color_palette())
-plt.show()
-
+# plt.show()
